81c0f6aba4cc661f2307ec147b121efb53013a5d
Archie <139076797+TeamArchie@users.noreply.github.com>
2023-10-15 16:20:17 -0600

    Add files via upload

---
+++ Notifiers/Prison.lua
@@ -0,0 +1,11 @@
+_77Watermark("Script developed by Team Archie! v2.0, notifications by Kinlei")
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+
+wait(3);
+
+Notify({
+Description = "Teleporting and giving...";
+Title = "ArchieHub | Prison Life";
+Duration = 3;
+});
