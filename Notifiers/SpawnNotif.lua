f2af885c48ec331befbfe913b76b155ad8e8a8c5
Archie <139076797+TeamArchie@users.noreply.github.com>
2023-12-31 12:41:20 -0600

    Create SpawnNotif.lua

---
+++ Notifiers/SpawnNotif.lua
@@ -0,0 +1,14 @@
+ Rayfield:Notify({
+   Title = "Spawned",
+   Content = "Your bike spawned correctly!",
+   Duration = 6.5,
+   Image = 11478378840,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+
+      end
+   },
+},
+})
