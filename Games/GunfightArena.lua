d02879c5bc72046aae0ea8798b0393bcde29e752
TeamArchie <139076797+TeamArchie@users.noreply.github.com>
2025-03-22 19:32:08 -0600

    Update GunfightArena.lua

--- Games/GunfightArena.lua
+++ Games/GunfightArena.lua
@@ -1,6 +1,5 @@
-wait(2)
 print("Phase 1")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/ScriptPhases/GunfightArenaPhase1"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/Games/GunfightArenaPhase1"))()
 wait(2)
 print("Phase 2")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/ScriptPhases/GunfightArenaPhase2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/Games/GunfightArenaPhase2"))()
