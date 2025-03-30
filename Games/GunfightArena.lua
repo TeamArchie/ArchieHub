faa4754e2bc91c7d6c84394d33c2fa4e64fa0498
TeamArchie <139076797+TeamArchie@users.noreply.github.com>
2025-03-30 00:34:07 -0600

    Update GunfightArena.lua

--- Games/GunfightArena.lua
+++ Games/GunfightArena.lua
@@ -1,5 +1,3 @@
-print("Loader: Phase 1")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/ScriptPhases/GunfightArena-Phase1"))()
-wait(1)
+print("Loader: Phase 1 is skipped due to Silent Aimbot issues + FPS Drop issues")
 print("Loader: Phase 2")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/ScriptPhases/GunfightArena-Phase2"))()
