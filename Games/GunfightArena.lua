317574a8ea4ccd5e8852684f162d1941476cc312
TeamArchie <139076797+TeamArchie@users.noreply.github.com>
2025-09-28 19:40:37 -0600

    Add files via upload

---
+++ Games/GunfightArena.lua
@@ -0,0 +1,3 @@
+print("Loader: Phase 1 is skipped due to Silent Aimbot issues + FPS Drop issues")
+print("Loader: Phase 2")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamArchie/ArchieHub/refs/heads/main/ScriptPhases/GunfightArena-Phase2"))()
